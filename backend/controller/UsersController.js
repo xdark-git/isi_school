0df57b89303299e06854c901b2407ae900ba3624
xdark-git <konteyemouhamadou@gmail.com>
2022-07-28 19:58:10 +0000

    fixing error while trying to add but created message still getting send after error

--- backend/controller/UsersController.js
+++ backend/controller/UsersController.js
@@ -17,68 +17,113 @@ let date = new Date();
 let unix = Date.parse(date);
 // let lienProfile = "User/" + unix;
 
-/**
- * we will first make sure that the email, username and telephone aren't
- * already been used and create the new user
+/*
+  1. Verify if email, username, telephone doesn't in all 3 collections (Professeur, Etudiant, Administration)
+  2. Verify if status id exist
+  3. Add the new user
  */
-export const createAdmin = (req, res) => {
-  Administration.find(
-    {
-      $or: [
-        { email: req.body.email },
-        { username: req.body.username },
-        { telephone: req.body.telephone },
-      ],
-    },
-    (reqError, data) => {
-      let email = "Ok";
-      let username = "Ok";
-      let telephone = "Ok";
-      if (data.length > 0) {
-        for (let i in data) {
-          if (data[i].email == req.body.email) email = "Conflict";
-          if (data[i].username == req.body.username) username = "Conflict";
-          if (data[i].telephone == req.body.telephone) telephone = "Conflict";
-        }
-        return res.status(409).json({
-          email: email,
-          username: username,
-          telephone: telephone,
-        });
-      } else {
-        // Trying to find if the status select exist and get the id
-        Status.findById(req.body.statusId).exec((error, data) => {
-          if (data.length < 0)
-            return res.status(204).json({ message: "No Content" });
-        });
 
-        try {
-          Administration.create(req.body).then(
-            res.status(201).json({ message: "Created" })
-          );
-        } catch (error) {
-          res.status(500).json({ message: "Internal Server Error" });
-        }
+/**
+ * create a new Administration
+ */
+export const createAdmin = async (req, res) => {
+  await Professeur.find({
+    $or: [
+      { email: req.body.email },
+      { username: req.body.username },
+      { telephone: req.body.telephone },
+    ],
+  }).exec((reqError, data) => {
+    let email = "Ok";
+    let username = "Ok";
+    let telephone = "Ok";
+    if (data.length > 0) {
+      for (let i in data) {
+        if (data[i].email == req.body.email) email = "Conflict";
+        if (data[i].username == req.body.username) username = "Conflict";
+        if (data[i].telephone == req.body.telephone) telephone = "Conflict";
       }
+      return res.status(409).json({
+        email: email,
+        username: username,
+        telephone: telephone,
+      });
     }
-  );
+    console.log(data);
+  });
+
+  await Etudiant.find({
+    $or: [
+      { email: req.body.email },
+      { username: req.body.username },
+      { telephone: req.body.telephone },
+    ],
+  }).exec((reqError, data) => {
+    let email = "Ok";
+    let username = "Ok";
+    let telephone = "Ok";
+    if (data.length > 0) {
+      for (let i in data) {
+        if (data[i].email == req.body.email) email = "Conflict";
+        if (data[i].username == req.body.username) username = "Conflict";
+        if (data[i].telephone == req.body.telephone) telephone = "Conflict";
+      }
+      return res.status(409).json({
+        email: email,
+        username: username,
+        telephone: telephone,
+      });
+    }
+  });
+
+  await Administration.find({
+    $or: [
+      { email: req.body.email },
+      { username: req.body.username },
+      { telephone: req.body.telephone },
+    ],
+  }).exec((reqError, data) => {
+    let email = "Ok";
+    let username = "Ok";
+    let telephone = "Ok";
+    if (data.length > 0) {
+      for (let i in data) {
+        if (data[i].email == req.body.email) email = "Conflict";
+        if (data[i].username == req.body.username) username = "Conflict";
+        if (data[i].telephone == req.body.telephone) telephone = "Conflict";
+      }
+      return res.status(409).json({
+        email: email,
+        username: username,
+        telephone: telephone,
+      });
+    } else {
+      // Trying to find if the status select exist and get the id
+      Status.findById(req.body.statusId).exec((error, statusData) => {
+        if (statusData == null)
+          return res.status(406).json({ message: "Not Acceptable Status" });
+        else {
+          try {
+            Administration.create(req.body).then(
+              res.status(201).json({ message: "Created" })
+            );
+          } catch (error) {
+            res.status(500).json({ message: "Internal Server Error" });
+          }
+        }
+      });
+    }
+  });
 };
 
-/*
-  First check if the username, email, numeroDeTel, identifiantProf
- aren't already used
- then check if the statusId exist in statusCollection
- if all verification passed successfully then create the newProf
+/**
+ * create a new Professuer
  */
 export const createProfesseur = (req, res) => {};
 
-/*
- First check if the username, email, numeroDeTel, numeroDeCarte
- aren't already used
- then check if the statusId exist in statusCollection
- if all verification passed successfully then create the newProf
+/**
+ * create a new Professuer
  */
-
 export const createEtudiant = (req, res) => {};
 /**
  * shuffle array to create a userlink
