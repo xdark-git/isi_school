0c9f11af88b484125a8e7b0a2f6412d07835e03b
xdark-git <konteyemouhamadou@gmail.com>
2022-07-29 16:23:58 +0000

    renaming files

--- backend/routes/createStatus.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createStatus} from "../controller/StatusController.js";
-
-const router = express.Router();
-
-router.post("/add", createStatus);
-
-export default router;
