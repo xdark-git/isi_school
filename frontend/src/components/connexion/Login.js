b14c33d0a85990eb75ca91a74988f166dcc4dcae
xdark-git <konteyemouhamadou@gmail.com>
2022-08-07 14:23:01 +0000

    changing from react hook to redux method for profiles

--- frontend/src/components/connexion/Login.js
+++ frontend/src/components/connexion/Login.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import { Button } from "react-bootstrap";
+
 import "./style.css";
+import { actions } from "../../store/profilOnClick";
 
 const Login = () => {
-  const [PROFIL_STYLE_ON_CLICK, SET_PROFIL_STYLE_ON_CLICK] = useState({
-    etudiant: "btn-profil checked",
-    professeur: "btn-profil",
-  });
   /*
       changing the profile style depending on the profil clicked
   */
+  const etudiantDefaultStyle = useSelector((state) => state.etudiant);
+  const professeurDefaultStyle = useSelector((state) => state.professeur);
+  const dispatch = useDispatch();
   const handleClick = (event) => {
-    if (event.currentTarget.textContent == "Professeur") {
-      SET_PROFIL_STYLE_ON_CLICK({ etudiant: "btn-profil", professeur: "btn-profil checked" });
+    if (event.currentTarget.textContent === "Professeur") {
+      dispatch(actions.professeur());
     }
-    if (event.currentTarget.textContent == "Etudiant") {
-      SET_PROFIL_STYLE_ON_CLICK({ etudiant: "btn-profil checked", professeur: "btn-profil" });
+    if (event.currentTarget.textContent === "Etudiant") {
+      dispatch(actions.etudiant());
     }
   };
 
@@ -38,17 +41,28 @@ const Login = () => {
 
         <form className="form" onSubmit={handleSubmit}>
           <div className="profil">
-            <div className={PROFIL_STYLE_ON_CLICK.etudiant} onClick={handleClick}>
+            <div
+              className={etudiantDefaultStyle}
+              onClick={handleClick}
+            >
               <span className="label">Etudiant</span>
             </div>
-            <div className={PROFIL_STYLE_ON_CLICK.professeur} onClick={handleClick}>
+            <div
+              className={professeurDefaultStyle}
+              onClick={handleClick}
+            >
               <span className="label">Professeur</span>
             </div>
           </div>
           <div className="section-input">
             <div className="input">
               <label for="email">Email</label>
-              <input type="text" placeholder="Votre email" id="email" className="email" />
+              <input
+                type="text"
+                placeholder="Votre email"
+                id="email"
+                className="email"
+              />
             </div>
             <div id="pwd" className="input">
               <label for="password">Password</label>
