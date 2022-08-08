6bf935af34eb5fcd640798ef05ad9f6a053326b6
xdark-git <konteyemouhamadou@gmail.com>
2022-08-08 16:25:21 +0000

    moving the const store to the index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import store from "./store/index.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reducers from "./reducers";
+
+const store = configureStore({
+  reducer: reducers,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
