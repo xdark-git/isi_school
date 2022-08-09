a194d1cd515eff39039ad207d5bd3a7a78f83451
xdark-git <konteyemouhamadou@gmail.com>
2022-08-09 11:47:20 +0000

    updating Login.js import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/connexion/Login";
+import Login from "./components/connexion/Login/Login";
 
 const App = () => {
   return (
