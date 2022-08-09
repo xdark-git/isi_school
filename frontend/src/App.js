118a14604f730b8021996cefd4385bb2ebe938ea
xdark-git <konteyemouhamadou@gmail.com>
2022-08-09 14:28:43 +0000

    importing Dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/connexion/Login/Login";
+import Dashboard from "./components/Dashbord";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
