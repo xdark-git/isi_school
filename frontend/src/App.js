4c93caddcf602d68013a8b3485131126f111d90f
xdark-git <konteyemouhamadou@gmail.com>
2022-08-09 17:20:39 +0000

    adding a catch all route and setting a missing component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/connexion/Login/Login";
 import Dashboard from "./components/Dashbord";
+import Missing from "./components/Missing";
 
 const App = () => {
   return (
@@ -10,6 +11,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* catch all */}
+          <Route path="*" element={<Missing />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/Missing.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Missing = () => {
+  return <div className="container">Page not Found</div>;
+};
+export default Missing;
