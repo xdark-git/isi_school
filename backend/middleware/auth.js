1209fb87591dfe65b7b45b44a414a3a3bdb61372
xdark-git <konteyemouhamadou@gmail.com>
2022-08-09 17:16:16 +0000

    correction

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.Authorization.split(" ")[1];
     let decodedData = jwt.verify(token, process.env.SECRET_JWT);
 
     req.userId = decodedData?.id;
