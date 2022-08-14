9f1adb703254f1fab9ca87e4bba4d456ed76a951
xdark-git <konteyemouhamadou@gmail.com>
2022-08-14 18:49:44 +0000

    importing and using cookie parser

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 //////////Routes//////////
 import userRouter from "./routes/usersRoutes.js";
 import statusRouter from "./routes/statusRoutes.js";
@@ -13,6 +14,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
