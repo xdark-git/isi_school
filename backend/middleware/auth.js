b5584080ec4f09b9aa3116a917e740cdc15737c7
xdark-git <konteyemouhamadou@gmail.com>
2022-08-15 17:21:49 +0000

    verifying and validating before authorisation

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,39 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-
+import verifyToken from "../functions/token/verifyToken.js";
+import verifyRefreshToken from "../functions/refreshToken/verifyRefreshToken.js";
+/*
+  we first verify if the token is sent or not 
+  then we check if the token sent is still valid or didn't expire
+  then we do the same for the refresh token
+*/
 const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.Authorization.split(" ")[1];
-    let decodedData = jwt.verify(token, process.env.SECRET_JWT);
-
-    req.userId = decodedData?.id;
-    next();
-  } catch (error) {
-    console.log(error);
+  if (!req?.cookies.token) {
+    return res.status(401).json({ message: "Missing cookie" });
   }
+
+  const token = req?.cookies?.token?.token;
+  const decodedToken  = await verifyToken(token);
+
+  if (decodedToken == false) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  const refreshToken = req?.cookies?.token?.refreshToken;
+  const decodedRefreshToken = await verifyRefreshToken(refreshToken)
+  
+  if( decodedRefreshToken == false){
+    return res.status(401).json({ message: "Invalid token" });
+  }
+  // let decodedData = jwt.verify(token, process.env.SECRET_JWT);
+  // console.log(decodedData);
+  
+  req.refreshToken = refreshToken;
+
+  next();
+  // const token = req.headers.Authorization.split(" ")[1];
+
+  // req.userId = decodedData?.id;
 };
 
 export default auth;
