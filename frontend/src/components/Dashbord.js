16739f5039f83bea31f6bddf2ce9548a304d623b
xdark-git <konteyemouhamadou@gmail.com>
2022-08-16 20:06:39 +0000

    removing unused code

--- frontend/src/components/Dashbord.js
+++ frontend/src/components/Dashbord.js
@@ -2,7 +2,7 @@ import React from "react";
 import Cookies from "universal-cookie";
 
 const Dashboard = () => {
-  const cookies = Cookies();
+  
 
   return <div className="container">dashboard</div>;
 };
