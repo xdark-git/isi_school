fc1c8e489377f8cd6b2a7d54f9e0eac8eb2436d3
xdark-git <konteyemouhamadou@gmail.com>
2022-08-16 17:42:32 +0000

    adding new constantes

--- frontend/src/constantes/index.js
+++ frontend/src/constantes/index.js
@@ -1,2 +1,14 @@
+//  Reducers actions
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+
+// cookies names
+export const USER_COOKIE_NAME = "dfhgoanvsm1233jdhfHD";
+
+//cookie options
+export const USER_COOKIE_OPTION = {
+  path: "/",
+  expires: new Date(Date.now() + 86400000),
+  domaine: toString(window?.location?.origin),
+  sameSite: "Strict",
+};
