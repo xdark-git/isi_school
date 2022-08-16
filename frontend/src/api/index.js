d3e76dbc8b9785c4570b01171c573cd8af411145
xdark-git <konteyemouhamadou@gmail.com>
2022-08-16 23:50:41 +0000

    renaming constantes

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -9,5 +9,5 @@ const API = axios.create({ baseURL: "http://localhost:5000/api" });
 // });
 
 export const signEtudiant = (formData) => API.post("/user/login/etudiant", formData);
-export const loginProfesseur = (formData) => API.post("/user/login/professeur", formData);
-export const loginAdministration = (formData) => API.post("/user/login/administration", formData);
+export const signProfesseur = (formData) => API.post("/user/login/professeur", formData);
+export const signAdministration = (formData) => API.post("/user/login/administration", formData);
