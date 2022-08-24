38115a750eac52676143ca063e368ff38b455dbc
xdark-git <konteyemouhamadou@gmail.com>
2022-08-24 01:41:31 +0000

    creating and styling the navbar

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/style.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <a className="logo" href="/nav">
+        ISI School
+      </a>
+      <div className="input">
+        <label htmlFor="search">Rechercher</label>
+        <input type="text" id="search" name="search" className="search" />
+      </div>
+    </header>
+  );
+};
+export default Navbar;
