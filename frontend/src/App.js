9974cd272cd083349aab1a166161e5f00fe939bb
xdark-git <konteyemouhamadou@gmail.com>
2022-08-24 01:40:56 +0000

    creating a temporary root for nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LoginAdmin from "./components/connexion/Admin/LoginAdmin";
 import Login from "./components/connexion/Login/Login";
 import Dashboard from "./components/Dashbord";
 import Missing from "./components/Missing";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Login />} />
           <Route path="/login/admin" element={<LoginAdmin />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/nav" element={<Navbar />} />
 
           {/* catch all */}
           <Route path="*" element={<Missing />} />
