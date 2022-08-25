814361764f881ffd0278cc088244794bc4745c21
xdark-git <konteyemouhamadou@gmail.com>
2022-08-25 17:10:20 +0000

    nav design

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,16 +3,27 @@ import "./css/style.css";
 
 const Navbar = () => {
   return (
-    <header>
-      <a className="logo" href="/nav">
-        ISI School
-      </a>
-      <div className="input">
-        <label htmlFor="search">Rechercher</label>
-        <input type="text" id="search" name="search" className="search" />
-        <button class="fa-solid fa-magnifying-glass search-icon"></button>
+    <div className="nav">
+      <header>
+        <a className="logo" href="/nav">
+          ISI School
+        </a>
+        <div className="input">
+          <label htmlFor="search">Rechercher</label>
+          <input type="text" id="search" name="search" className="search" />
+          <button class="fa-solid fa-magnifying-glass search-icon"></button>
+        </div>
+      </header>
+      <div className="profil">
+        <div className="user-infos">
+          {/* <div className="user-photo"></div> */}
+          <img src={process.env.PUBLIC_URL +"/img/user/default.jpg"} />
+          <div className="user-title"></div>
+          <div className="user-email"></div>
+          <button>Se deconnecter</button>
+        </div>
       </div>
-    </header>
+    </div>
   );
 };
 export default Navbar;
