2a296920eb8ad8e5d164a7252e79c816d86fe0a7
xdark-git <konteyemouhamadou@gmail.com>
2022-08-29 23:41:46 +0000

    updating Navbar code and css

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./asset/css/style.css";
 
 const Navbar = () => {  
   return (
-    <main>
+    <div>
       <header>
         {/* <a className="logo" href="/nav">
           ISI School
@@ -41,28 +41,7 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-      <div className="component">
-        <div className="new-search">
-          <div className="new">
-            <i class="fa-solid fa-plus"></i>
-            <div>nouvelle classe</div>
-          </div>
-          <div className="search">
-            <form>
-              <input
-                type="text"
-                id="search"
-                placeholder="Recherche"
-                name="search"
-                className="search"
-              />
-              <button class="fa-solid fa-magnifying-glass search-icon"></button>
-            </form>
-          </div>
-        </div>
-        <div className="content"></div>
-      </div>
-    </main>
+    </div>
   );
 };
 export default Navbar;
