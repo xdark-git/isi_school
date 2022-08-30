1df19217806a3956aac81261b999f44c9204351c
xdark-git <konteyemouhamadou@gmail.com>
2022-08-30 19:07:01 +0000

    displaying content using cookie

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,8 +1,13 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Cookies from "universal-cookie";
 import "./asset/css/style.css";
 
-const Navbar = () => {  
+import { USER_DATA_COOKIE_NAME } from "../../constantes";
+
+const Navbar = () => {
+  const cookies = new Cookies();
+  const user = cookies.get(USER_DATA_COOKIE_NAME);
+
   return (
     <div>
       <header>
@@ -12,8 +17,8 @@ const Navbar = () => {
         <div className="page-name">Classe</div>
         <div className="profile">
           <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-          <div className="user-name">Test</div>
-          <i class="fa-solid fa-caret-down"></i>
+          <div className="user-name">{user?.prenom}</div>
+          <i className="fa-solid fa-caret-down"></i>
         </div>
       </header>
       <nav>
@@ -23,22 +28,32 @@ const Navbar = () => {
             <i className="fa-solid fa-gear fa-lg"></i>
             <div>Profil</div>
           </div>
-          <div className="classes checked">
-            <i className="fa-solid fa-building fa-lg"></i>
-            <div>classes</div>
-          </div>
+          {user?.status === "Administrateur" && (
+            <div className="classes checked">
+              <i className="fa-solid fa-building fa-lg"></i>
+              <div>classes</div>
+            </div>
+          )}
           <div className="cours">
             <i className="fa-solid fa-file-lines fa-lg"></i>
             <div>Cours</div>
           </div>
+          {user?.status === "Administrateur" && (
+            <div className="informations">
+              <i className="fa-solid fa-bell fa-lg"></i>
+              <div>Informations</div>
+            </div>
+          )}
           <div className="informations">
             <i className="fa-solid fa-bell fa-lg"></i>
             <div>Informations</div>
           </div>
-          <div className="admin-users">
-            <i class="fa-solid fa-users fa-lg"></i>
-            <div>utilisateurs</div>
-          </div>
+          {user?.status === "Administrateur" && (
+            <div className="admin-users">
+              <i className="fa-solid fa-users fa-lg"></i>
+              <div>utilisateurs</div>
+            </div>
+          )}
         </div>
       </nav>
     </div>
