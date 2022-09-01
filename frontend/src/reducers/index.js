c5d2f1952f8ccc3fac2445a477f44ebc3f2b70ac
xdark-git <konteyemouhamadou@gmail.com>
2022-09-01 01:14:26 +0000

    import new reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { menuDialogReducer } from "./dialogs";
 
 import { profileSelectedReducer, signinUserReducer } from "./login";
 
 export default combineReducers({
   profile: profileSelectedReducer,
   signin: signinUserReducer,
+  stateMenuDialog: menuDialogReducer
 });
