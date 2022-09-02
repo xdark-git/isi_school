de934761c12b74247f8d3b8df3b74df5b5fee724
xdark-git <konteyemouhamadou@gmail.com>
2022-09-02 14:01:18 +0000

    creating and styling button annuler

--- frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
+++ frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeNewClassDialog } from "../../../../constantes";
 import "../style.css";
 
 const NewClassDialog = () => {
+  const dispatch = useDispatch();
   window.onclick = function (event) {
     var nouvelleClasseDiv = document.getElementById("nouvelleClasse");
-    if (event.target == nouvelleClasseDiv) {
-      console.log(event.target);
+    if (event.target === nouvelleClasseDiv) {
+      dispatch({ type: closeNewClassDialog });
     }
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  const closeDialog = () => {
+    dispatch({ type: closeNewClassDialog });
+    console.log("okay");
+  };
+
   return (
     <div id="nouvelleClasse" className="nouvelle-classe">
       <div className="nouvelle-classe-contenu">
         <div className="title">Nouvelle Classe</div>
         <div className="description">Merci de donner le nom de la nouvelle classe </div>
-        <form>
-          <input placeholder="Nom"/>
-          <button className="annuler">Annuler</button>
-          <button className="ajouter">Ajouter</button>
+        <form  onSubmit={handleSubmit}>
+          <input placeholder="Nom" />
+          <button className="ajouter" type="submit">
+            Ajouter
+          </button>
+          <button className="reset" type="reset">
+            Tout effacer
+          </button>
         </form>
+        <button className="annuler">Annuler</button>
       </div>
     </div>
   );
