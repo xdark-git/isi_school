e142957afa64d2e47a860fc708ad29f5fe0114b2
xdark-git <konteyemouhamadou@gmail.com>
2022-09-02 16:21:53 +0000

    creating an interceptor to send token in every req

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,15 @@
 import axios from "axios";
+
+import { USER_TOKEN_LOCAL_STORAGE_NAME } from "../../constantes";
+
 const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
-// API.interceptors.request.use((req) => {
-//   if (localStorage.getItem("profile")) {
-//     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile").token)}`;
-//   }
-//   return req;
-// });
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem(USER_TOKEN_LOCAL_STORAGE_NAME)) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem(USER_TOKEN_LOCAL_STORAGE_NAME)}`;
+  }
+  return req;
+});
 
 export const signEtudiant = (formData) => API.post("/user/login/etudiant", formData);
 export const signProfesseur = (formData) => API.post("/user/login/professeur", formData);
