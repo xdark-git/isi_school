3708963f535dd2cc695785a634f297ca40065018
xdark-git <konteyemouhamadou@gmail.com>
2022-09-03 14:55:06 +0000

    adding a get method

--- backend/routes/classeRoutes.js
+++ backend/routes/classeRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createClasse } from "../controller/ClasseController.js";
+import { createClasse, getAll } from "../controller/ClasseController.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/create", auth, createClasse);
+router.get("/", auth, getAll)
 
 export default router;
