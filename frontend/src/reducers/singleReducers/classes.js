c29b2209b5547c95b59044ef4a6180992fed7d10
xdark-git <konteyemouhamadou@gmail.com>
2022-09-04 00:38:19 +0000

    cleaning code

--- frontend/src/reducers/singleReducers/classes.js
+++ frontend/src/reducers/singleReducers/classes.js
@@ -3,7 +3,6 @@ import { GETALLCLASSES } from "../../constantes";
 export const getAllClassesReducers = (state = [], action) => {
   switch (action.type) {
     case GETALLCLASSES:
-     
       return action?.data;
     default:
       return state;
