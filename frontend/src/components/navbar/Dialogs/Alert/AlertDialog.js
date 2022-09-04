903f11c47cfa296a8bc87805a08fb788a165f88f
xdark-git <konteyemouhamadou@gmail.com>
2022-09-04 16:18:14 +0000

    using only one div instead of two

--- frontend/src/components/navbar/Dialogs/Alert/AlertDialog.js
+++ frontend/src/components/navbar/Dialogs/Alert/AlertDialog.js
@@ -4,21 +4,17 @@ import { useSelector } from "react-redux";
 
 const AlertDialog = () => {
   const message = useSelector((state) => state?.stateAlertDialog?.message);
-  const typeMessage = useSelector((state) => state?.stateAlertDialog?.typeMessage);
   return (
     <div className="alert">
-      {typeMessage === "ERROR" && (
-        <div className="warning">
-          <i className="fa-sharp fa-solid fa-exclamation fa-xl"></i>
-          <span>{message}</span>
-        </div>
-      )}
-      {typeMessage === "SUCCESS" && (
+      <div className="warning">
+        <span>{message}</span>
+      </div>
+      {/* {typeMessage === "SUCCESS" && (
         <div className="success">
           <i className="fa-solid fa-circle-check fa-lg"></i>
           <span>Created successfully</span>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
