be6c2d1df691f84e1df22e76cab56a5b684533d1
xdark-git <konteyemouhamadou@gmail.com>
2022-09-05 16:13:55 +0000

    creating a new path if an classe is found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
 import LoginAdmin from "./components/connexion/Admin/LoginAdmin";
 import Login from "./components/connexion/Login/Login";
 import Dashboard from "./components/Dashbord";
 import Missing from "./components/Missing";
 import Classes from "./components/classes/Classes";
+import DisplayOneClasse from "./components/classes/DisplayOne/DisplayOneClasse";
 
 const App = () => {
+
+  const lienClasse = useSelector((state) => state?.classe?.lien);
   return (
     <div>
       <BrowserRouter>
@@ -15,6 +20,7 @@ const App = () => {
           <Route path="/login/admin" element={<LoginAdmin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/classes" element={<Classes />} />
+          <Route path={"/classes/" + lienClasse} element={<DisplayOneClasse />} />
 
           {/* catch all */}
           <Route path="*" element={<Missing />} />
