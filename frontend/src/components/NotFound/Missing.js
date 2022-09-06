39b4e32a6c65e24b5b2a7bbfa1341211a42444f9
xdark-git <konteyemouhamadou@gmail.com>
2022-09-06 02:12:38 +0000

    removing unused codes

--- frontend/src/components/NotFound/Missing.js
+++ frontend/src/components/NotFound/Missing.js
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
 import Navbar from "../navbar/Navbar";
 import "./style.css";
 
 const Missing = () => {
-  const location = useLocation();
   const [display, setDisplay] = useState();
   const classeStatus = useSelector((state) => state?.classe?.opened);
 
   useEffect(() => {
     const pathName = window.location.pathname.split("/")[1];
     const param = window.location.pathname.split("/")[2];
-    console.log(param)
+    console.log(param);
     if (pathName === "classes" && param) {
       if (classeStatus === false) setDisplay(true);
     }
-    
-    if(pathName !== "classes"){
-      setDisplay(true)
+
+    if (pathName !== "classes") {
+      setDisplay(true);
     }
-    console.log(classeStatus)
+    console.log(classeStatus);
     // console.log();
   }, [classeStatus]);
- 
+
   return (
     <main>
       <Navbar />
