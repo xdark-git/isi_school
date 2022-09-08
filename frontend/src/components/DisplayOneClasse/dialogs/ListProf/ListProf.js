4f4c3c828525f6056b75a381f1d8807068be29fc
xdark-git <konteyemouhamadou@gmail.com>
2022-09-08 00:45:42 +0000

    responsive and code update

--- frontend/src/components/DisplayOneClasse/dialogs/ListProf/ListProf.js
+++ frontend/src/components/DisplayOneClasse/dialogs/ListProf/ListProf.js
@@ -5,75 +5,27 @@ const ListProf = () => {
   return (
     <div id="listing" className="listing">
       <div className="listing-header">
-        <i class="fa-solid fa-xmark fa-xl"></i>
-        <span className="listing-header-title">Professeurs</span>
+        <div>
+          <i class="fa-solid fa-xmark fa-xl"></i>
+          <span className="listing-header-title">Professeurs</span>
+        </div>
         <span className="listing-header-button">
-          <button className="btn-ajouter">Ajouter</button>
-          <button className="btn-supprimer">Supprimer</button>
+          <button type="submit" className="btn-ajouter">
+            Ajouter
+          </button>
+          <button type="submit" className="btn-supprimer">
+            Supprimer
+          </button>
         </span>
       </div>
       <div className="listing-content">
         <ul>
           <li>
             <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
-          </li> <li>
-            <img src={process.env.PUBLIC_URL + "/img/user/default.jpg"} alt="profil utilisateur" />
-            <span>Test test</span>
-            <input type="checkbox"/>
+            <div>
+              <label htmlFor="user-checkbox">Test test</label>
+              <input id="user-checkbox" type="checkbox" />
+            </div>
           </li>
         </ul>
       </div>
