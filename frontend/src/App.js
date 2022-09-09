2513f4f1991d367e827551e8ebd52618ca37f047
xdark-git <konteyemouhamadou@gmail.com>
2022-09-09 17:44:41 +0000

    deleting unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginAdmin from "./components/connexion/Admin/LoginAdmin";
 import Login from "./components/connexion/Login/Login";
-import Dashboard from "./components/Dashbord";
 import Missing from "./components/NotFound/Missing";
 import Classes from "./components/classes/Classes";
 import DisplayOneClasse from "./components/DisplayOneClasse/DisplayOneClasse";
@@ -16,7 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login/admin" element={<LoginAdmin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           <Route path="/classes" element={<Classes />} />
           <Route path={"/classes/" + lienClasse} element={<DisplayOneClasse />} />
 
