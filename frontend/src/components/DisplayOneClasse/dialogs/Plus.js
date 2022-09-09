e32de13e403960207316e686fa90f2387a147a60
xdark-git <konteyemouhamadou@gmail.com>
2022-09-09 01:50:02 +0000

    opening listProfEtudiantdialog onclick

--- frontend/src/components/DisplayOneClasse/dialogs/Plus.js
+++ frontend/src/components/DisplayOneClasse/dialogs/Plus.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { openListProfEtudiantDialog } from "../../../constantes";
 
 const Plus = (changeState, state) => {
+  const dispatch = useDispatch();
+  const listProf = () => {
+    dispatch({ type: openListProfEtudiantDialog, profile: "Professeur" });
+  };
+  const listEtudiant = () => {
+    dispatch({ type: openListProfEtudiantDialog, profile: "Etudiant" });
+  };
   return (
     <ul id="plusDialog" className="dialog">
-      <li className="display-prof">Liste des professeurs</li>
-      <li className="display-etudiant">Liste des etudiants</li>
+      <li className="display-prof" onClick={listProf}>
+        Liste des professeurs
+      </li>
+      <li className="display-etudiant" onClick={listEtudiant}>
+        Liste des etudiants
+      </li>
       <li>Supprimer la classe</li>
     </ul>
   );
