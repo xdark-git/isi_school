7899650d015eff997aceba7184b96c052d2a3405
xdark-git <konteyemouhamadou@gmail.com>
2022-09-10 15:40:27 +0000

    updating code

--- backend/controller/ClasseController.js
+++ backend/controller/ClasseController.js
@@ -7,7 +7,7 @@ import generateUniqueLink from "../functions/generateUniqueLink.js";
 export const createClasse = async (req, res) => {
   try {
     //checking if the user still exist
-    const user = await Administration.findById(req?.user?.id);
+    const user = await Administration.findById(req?.user?.id).where("isDeleted").equals(false);
     if (!user) {
       return res.status(401).json({ message: "Access denied" });
     }
@@ -20,7 +20,7 @@ export const createClasse = async (req, res) => {
       });
     }
     //check if there isn't a classe who already had the same name
-    const existingClasse = await Classe.findOne(req.body);
+    const existingClasse = await Classe.findOne(req.body).where("isDeleted").equals(false);
 
     if (existingClasse) {
       return res.status(409).json({ message: "A classe with the same name already exist" });
@@ -29,7 +29,12 @@ export const createClasse = async (req, res) => {
     //classe creation
     const lien = generateUniqueLink(req?.user?.id);
     const classe = await Classe.create({ nom: req.body.nom, lien: lien, admin_id: req.user.id });
-    return res.status(200).json(classe);
+    // cleaning the data to send
+    const newClasse = await Classe.findById(classe["_id"])
+      .where("isDeleted")
+      .equals(false)
+      .select("-__v -isDeleted");
+    return res.status(200).json(newClasse);
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong." });
   }
@@ -38,16 +43,19 @@ export const createClasse = async (req, res) => {
 export const getAll = async (req, res) => {
   try {
     //checking if the user still exist
-    const user = await Administration.findById(req?.user?.id);
+    const user = await Administration.findById(req?.user?.id).where("isDeleted").equals(false);
     if (!user) {
       return res.status(401).json({ message: "Access denied" });
     }
 
-    const classes = await Classe.find({});
+    const classes = await Classe.find({})
+      .where("isDeleted")
+      .equals(false)
+      .select("-__v -isDeleted");
 
     return res.status(200).json(classes);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return res.status(500).json({ message: "Something went wrong." });
   }
 };
@@ -55,18 +63,23 @@ export const getAll = async (req, res) => {
 export const getOne = async (req, res) => {
   try {
     //checking if the user still exist
-    const user = await Administration.findById(req?.user?.id);
+    const user = await Administration.findById(req?.user?.id).where("isDeleted").equals(false);
     if (!user) {
       return res.status(401).json({ message: "Access denied" });
     }
 
-    const classe = await Classe.findById(req?.params?._id);
+    const classe = await Classe.findById(req?.params?._id)
+      .where("isDeleted")
+      .equals(false)
+      .select("-__v -isDeleted");
+
     if (!classe) {
       return res.status(404).json({ message: "Not found" });
     }
 
     return res.status(200).json(classe);
   } catch (error) {
-    return res.status(400).json({ message: "malformed request syntax." });
+    // return res.status(400).json({ message: "malformed request syntax." });
+    return res.status(404).json({ message: "Not Found" });
   }
 };
