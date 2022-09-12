046a3f3c7f1c5e3908013f41c981338ae21bb65e
xdark-git <konteyemouhamadou@gmail.com>
2022-09-12 16:49:39 +0000

    sending data on click

--- frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
+++ frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createClasse } from "../../../../actions/classe/createUpdateClasses";
 import { closeNewClassDialog } from "../../../../constantes";
 import "../style.css";
 
 const NewClassDialog = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   window.onclick = function (event) {
     var nouvelleClasseDiv = document.getElementById("nouvelleClasse");
     if (event.target === nouvelleClasseDiv) {
       dispatch({ type: closeNewClassDialog });
     }
   };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
   const closeDialog = () => {
     dispatch({ type: closeNewClassDialog });
     console.log("okay");
+  };
+
+  const [nomClasse, setNomClasse] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log(nomClasse);
+    dispatch(createClasse({nom: nomClasse}, navigate))
   };
 
   return (
@@ -25,7 +32,13 @@ const NewClassDialog = () => {
         <div className="title">Nouvelle Classe</div>
         <div className="description">Merci de donner le nom de la nouvelle classe </div>
         <form onSubmit={handleSubmit}>
-          <input placeholder="Nom" />
+          <input
+            type="text"
+            name="nom"
+            placeholder="Nom"
+            value={nomClasse}
+            onChange={(e) => setNomClasse(e.target.value)}
+          />
           <button className="ajouter" type="submit">
             Ajouter
           </button>
