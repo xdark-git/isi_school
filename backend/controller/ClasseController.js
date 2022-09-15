ed837e1c502f708c3369fc4cdf56ef6bcedbbc04
xdark-git <konteyemouhamadou@gmail.com>
2022-09-15 00:40:54 +0000

    adding a new method to add prof in a classe

--- backend/controller/ClasseController.js
+++ backend/controller/ClasseController.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { Classe } from "../model/Classe.js";
 import { Administration } from "../model/Administration.js";
+import { Professeur } from "../model/Professeur.js";
 import { Status } from "../model/Status.js";
 import generateUniqueLink from "../functions/generateUniqueLink.js";
-import { handleClasseError } from "../functions/handleError.js";
+import { handleClasseError, handleModelIdOnFindError } from "../functions/handleError.js";
+import { Cours } from "../model/Cours.js";
 
 export const createClasse = async (req, res) => {
   try {
@@ -80,10 +82,59 @@ export const getOne = async (req, res) => {
     if (!classe) {
       return res.status(404).json({ message: "Introuvable" });
     }
+    //fetching all cours that has a cours_id equals to classe id
+    const existingCoursInThisClasse = await Cours.find({}).where({
+      isDeleted: false,
+      classe_id: classe["_id"],
+    });
+    console.log(existingCoursInThisClasse);
 
     return res.status(200).json(classe);
   } catch (error) {
     // return res.status(400).json({ message: "malformed request syntax." });
     return res.status(404).json({ message: "Introuvable" });
+  }
+};
+
+export const addNewProf = async (req, res) => {
+  try {
+    //checking if the user still exist
+    const user = await Administration.findById(req?.user?.id).where("isDeleted").equals(false);
+    if (!user) {
+      return res.status(401).json({ message: "Accès non autorisé" });
+    }
+    //verifying if the prof exist
+    const existingProf = await Professeur.findById(req?.body?.prof_id)
+      .where("isDeleted")
+      .equals(false);
+    if (!existingProf) {
+      return res
+        .status(401)
+        .json({ errors: { message: "Le professeur sélectionné n'existe pas" } });
+    }
+    //verifying if the class still exist and the prof isn't already added
+    const existingClasse = await Classe.findById(req?.params?._id).where("isDeleted").equals(false);
+    if (!existingClasse) {
+      return res.status(401).json({ errors: { message: "La classe sélectionnée n'existe pas" } });
+    }
+    if (existingClasse?.profs_id.includes(req?.body?.prof_id) == true) {
+      return res
+        .status(401)
+        .json({ errors: { message: "Le professeur existe déjà dans la classe" } });
+    }
+    //updating the classe
+    existingClasse?.profs_id.push(req?.body?.prof_id);
+    const isItUpdated = await existingClasse.save();
+    return res.status(200).json({ message: "Professeur ajouter avec succès" });
+  } catch (errors) {
+    const error = handleModelIdOnFindError(errors);
+
+    if (error == true && errors.message.includes("Classe"))
+      return res.status(401).json({ errors: { message: "La classe sélectionnée n'existe pas" } });
+
+    if (error == true && errors.message.includes("Professeur"))
+      return res
+        .status(401)
+        .json({ errors: { message: "Le professeur sélectionné n'existe pas" } });
   }
 };
--- backend/routes/classeRoutes.js
+++ backend/routes/classeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createClasse, getAll, getOne } from "../controller/ClasseController.js";
+import { addNewProf, createClasse, getAll, getOne } from "../controller/ClasseController.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create", auth, createClasse);
 router.get("/", auth, getAll);
 router.get("/:_id", auth, getOne);
+router.put("/addprof/:_id", auth, addNewProf);
 
 export default router;
