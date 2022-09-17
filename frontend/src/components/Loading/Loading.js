4db2fd8249a13d7b768a2168a3d8df64fe05a948
xdark-git <konteyemouhamadou@gmail.com>
2022-09-17 20:29:14 +0000

    new component to show loading

---
+++ frontend/src/components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./asset/style.css";
+
+const Loading = () => {
+  return (
+    <div className="div">
+        <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
