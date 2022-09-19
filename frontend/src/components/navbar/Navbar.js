b2fb54a333deb4f83195a8501c456b8c2c93d1af
xdark-git <konteyemouhamadou@gmail.com>
2022-09-19 01:03:29 +0000

    deleting unused code

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import Cookies from "universal-cookie";
