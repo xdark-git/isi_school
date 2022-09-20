da2ac4a8307c765bf93ec652b3f610e447885775
xdark-git <konteyemouhamadou@gmail.com>
2022-09-20 00:19:24 +0000

    new condition on what to display and how to handle things

--- frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
+++ frontend/src/components/classes/Dialogs/NewClasse/NewClassDialog.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { createClasse } from "../../../../actions/classe/createUpdateClasses";
 import { closeNewClassDialog, CREATIONCLASSEDIALOGCLOSED } from "../../../../constantes";
 import "../style.css";
 
-const NewClassDialog = () => {
+const NewClassDialog = (action) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   window.onclick = function (event) {
@@ -23,41 +23,74 @@ const NewClassDialog = () => {
   };
 
   const [nomClasse, setNomClasse] = useState("");
+  const nomClasseToUpdate = useSelector((state) => state?.classe?.data?.classe?.nom);
+  const error = useSelector((state) => state?.createClasse?.errors);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log(nomClasse);
-    dispatch(createClasse({ nom: nomClasse }, navigate));
+    if (action?.objectif === "Creation") dispatch(createClasse({ nom: nomClasse }, navigate));
   };
-  const error = useSelector((state) => state?.createClasse?.errors);
 
-  return (
-    <div id="nouvelleClasse" className="nouvelle-classe">
-      <div className="nouvelle-classe-contenu">
-        <div className="title">Nouvelle Classe</div>
-        <div className="description">Merci de donner le nom de la nouvelle classe </div>
-        <form onSubmit={handleSubmit}>
-          {error?.message && <div className="error">{error?.message}</div>}
-          {error?.nom && <div className="error">{error?.nom}</div>}
-          <input
-            type="text"
-            name="nom"
-            placeholder="Nom"
-            value={nomClasse}
-            onChange={(e) => setNomClasse(e.target.value)}
-          />
-          <button className="ajouter" type="submit">
-            Ajouter
+  if (action?.objectif === "Creation") {
+    return (
+      <div id="nouvelleClasse" className="nouvelle-classe">
+        <div className="nouvelle-classe-contenu">
+          <div className="title">Nouvelle Classe</div>
+          <div className="description">Merci de donner le nom de la nouvelle classe </div>
+          <form onSubmit={handleSubmit}>
+            {error?.message && <div className="error">{error?.message}</div>}
+            {error?.nom && <div className="error">{error?.nom}</div>}
+            <input
+              type="text"
+              name="nom"
+              placeholder="Nom"
+              value={nomClasse}
+              onChange={(e) => setNomClasse(e.target.value)}
+            />
+            <button className="ajouter" type="submit">
+              Ajouter
+            </button>
+            <button className="reset" type="reset">
+              Tout effacer
+            </button>
+          </form>
+          <button className="annuler" onClick={closeDialog}>
+            Annuler
           </button>
-          <button className="reset" type="reset">
-            Tout effacer
-          </button>
-        </form>
-        <button className="annuler" onClick={closeDialog}>
-          Annuler
-        </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (action?.objectif === "Modification") {
+    return (
+      <div id="nouvelleClasse" className="nouvelle-classe">
+        <div className="nouvelle-classe-contenu">
+          <div className="title">Modification Classe</div>
+          <div className="description">Merci de donner le nouveau nom de la classe </div>
+          <form onSubmit={handleSubmit}>
+            {error?.message && <div className="error">{error?.message}</div>}
+            {error?.nom && <div className="error">{error?.nom}</div>}
+            <input
+              type="text"
+              name="nom"
+              placeholder="Nom"
+              value={nomClasseToUpdate}
+              onChange={(e) => setNomClasse(e.target.value)}
+            />
+            <button className="ajouter" type="submit">
+              Ajouter
+            </button>
+            <button className="reset" type="reset">
+              Tout effacer
+            </button>
+          </form>
+          <button className="annuler" onClick={closeDialog}>
+            Annuler
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default NewClassDialog;
