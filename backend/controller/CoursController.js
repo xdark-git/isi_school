89939592e109ad31794b37996c05daf5f13037e2
xdark-git <konteyemouhamadou@gmail.com>
2022-09-23 16:38:14 +0000

    new metho to get one cours

--- backend/controller/CoursController.js
+++ backend/controller/CoursController.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import { Cours } from "../model/Cours.js";
+import { Contenu } from "../model/Contenu.js";
 import { Classe } from "../model/Classe.js";
 import { Administration } from "../model/Administration.js";
 import { Professeur } from "../model/Professeur.js";
-import { Status } from "../model/Status.js";
+import { Etudiant } from "../model/Etudiant.js";
 import generateUniqueLink from "../functions/generateUniqueLink.js";
-import { handleClasseError } from "../functions/handleError.js";
+import { handleModelIdOnFindCours } from "../functions/handleErrors/handleCoursError.js";
 
 export const createCours = async (req, res) => {
   try {
@@ -80,4 +81,65 @@ export const createCours = async (req, res) => {
 };
 
 export const getAll = (req, res) => {};
-export const getOne = (req, res) => {};
+export const getOne = async (req, res) => {
+  try {
+    //checking if the user still exist
+    let user;
+    if (req?.user?.status == "Administrateur") {
+      user = await Administration.findById(req?.user?.id).where("isDeleted").equals(false);
+    }
+
+    if (req?.user?.status == "Professeur") {
+      user = await Professeur.findById(req?.user?.id).where("isDeleted").equals(false);
+    }
+
+    if (req?.user?.status == "Etudiant") {
+      user = await Etudiant.findById(req?.user?.id).where("isDeleted").equals(false);
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: "Accès non autorisé" });
+    }
+
+    const cours = await Cours.findById(req?.params?._id)
+      .where({ isDeleted: false })
+      .select("-__v -isDeleted");
+
+    if (!cours) {
+      return res.status(404).json({ message: "Cours introuvable" });
+    }
+    // get the classe and verify if not deleted
+    const classe = await Classe.findById(cours?.classe_id).where({ isDeleted: false });
+    if (!classe) {
+      return res.status(404).json({ message: "Classe supprimée" });
+    }
+    
+    //check if classe_id exist in user
+    // if it exist and is an array, check if it includes the classe id
+    // if it exist but not an array, check if it's equall to classe id
+    if (
+      "classe_id" in user &&
+      Array.isArray(user?.classe_id) &&
+      !user?.classe_id.includes(classe?._id.toString())
+    ) {
+      return res.status(401).json({ message: "Accès non autorisé" });
+    }
+    if (
+      "classe_id" in user &&
+      !Array.isArray(user?.classe_id) &&
+      user?.classe_id != classe?._id.toString()
+    ) {
+      return res.status(401).json({ message: "Accès non autorisé" });
+    }
+
+    // fetching all contenu of the classe
+    const contenu = await Contenu.find({})
+      .where({ isDeleted: false, cours_id: cours?.id })
+      .select("-__v -isDeleted");
+
+    return res.status(201).json({ cours: cours, contenus: contenu });
+  } catch (error) {
+    console.log(error);
+    const errors = handleModelIdOnFindCours(error, res);
+  }
+};
