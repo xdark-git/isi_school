eac3f25b907a49669bc1c8c114bb9646729f9cc6
xdark-git <konteyemouhamadou@gmail.com>
2022-09-24 22:29:37 +0000

    waiting for database response before displaying

--- frontend/src/components/NotFound/Missing.js
+++ frontend/src/components/NotFound/Missing.js
@@ -7,29 +7,33 @@ import "./asset/css/missing.css";
 const Missing = () => {
   const [display, setDisplay] = useState();
   const classeStatus = useSelector((state) => state?.classe?.opened);
+  const coursStatus = useSelector((state) => state?.cours?.opened);
 
   useEffect(() => {
     const pathName = window.location.pathname.split("/")[1];
     const param = window.location.pathname.split("/")[2];
     if (pathName === "classes" && param) {
       if (classeStatus === false) setDisplay(true);
-    }
-
-    if (pathName !== "classes") {
+    } else if (pathName === "cours" && param) {
+      if (coursStatus === false) setDisplay(true);
+    } else {
       setDisplay(true);
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [classeStatus]);
 
   return (
     <main>
       <Navbar />
-      {display === true && (
+      {display === true ? (
         <div className="not-found">
           <div className="title">Oups !</div>
           <div className="description">La page que vous recherchez semble introuvable</div>
         </div>
+      ) : (
+        <Loading />
       )}
-      {display !== true && <Loading />}
     </main>
   );
 };
