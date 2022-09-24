1885595afdbb5e21e90575bd37f5946dfa1b3078
xdark-git <konteyemouhamadou@gmail.com>
2022-09-24 00:24:58 +0000

    display one cours component

---
+++ frontend/src/components/cours/DisplayOne/DisplayOneCours.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../navbar/Navbar";
+
+const DisplayOneCours = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+
+export default DisplayOneCours;
