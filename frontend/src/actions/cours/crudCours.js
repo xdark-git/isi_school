b6194d23b7e57755b4bbf254a492b6996f16fa1a
xdark-git <konteyemouhamadou@gmail.com>
2022-09-24 00:23:11 +0000

    action to fetch oneCours

---
+++ frontend/src/actions/cours/crudCours.js
@@ -0,0 +1,14 @@
+import * as api from "../../api";
+import { closeLoaderComponent, DISPLAYONECOURS } from "../../constantes";
+
+export const getOne = (id, navigate) => async (dispatch) => {
+  try {
+    const cours = await api.getOneCours(id);
+    const data = cours?.data;
+    dispatch({ type: DISPLAYONECOURS, data, opened: true });
+    navigate("/cours/" + data?.cours?._id);
+    dispatch({ type: closeLoaderComponent });
+  } catch (error) {
+    console.log(error);
+  }
+};
