bee7fd39d6006032d6c74ed135ee97124b8a8ea3
xdark-git <konteyemouhamadou@gmail.com>
2022-09-26 22:32:20 +0000

    building template

--- frontend/src/components/cours/DisplayOne/DisplayOneCours.js
+++ frontend/src/components/cours/DisplayOne/DisplayOneCours.js
@@ -1,10 +1,48 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Navbar from "../../navbar/Navbar";
+import "./asset/style.css";
 
 const DisplayOneCours = () => {
+  const cours = useSelector((state) => state?.cours?.data?.cours);
   return (
     <main>
-      <Navbar />
+      <Navbar path={cours?.titre} />
+      <div className="component">
+        <div className="display-search">
+          <div className="display">
+            <div id="plus" className="more">
+              <i className="fa-solid fa-ellipsis-vertical fa-lg"></i>
+              <span>Plus</span>
+            </div>
+          </div>
+          <div className="search">
+            <form>
+              <input
+                type="text"
+                id="search"
+                placeholder="Recherche"
+                name="search"
+                className="search"
+              />
+              <button className="fa-solid fa-magnifying-glass search-icon"></button>
+            </form>
+          </div>
+        </div>
+        <div className="content-cours">
+          <ul>
+            <li>
+              <span className="cours-number">Numéro</span>
+              <span className="cours-title">titre</span>
+              <span className="cours-date">date de création</span>
+              <span className="cours-options">
+                <i className="fa-solid fa-download"></i>
+                <i className="fa-solid fa-eye"></i>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 };
