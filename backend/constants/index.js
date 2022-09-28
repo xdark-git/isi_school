866576eee56f3796581feaee5d49b2be6a22948a
xdark-git <konteyemouhamadou@gmail.com>
2022-09-28 17:32:10 +0000

    new contante containing list of filetype

---
+++ backend/constants/index.js
@@ -0,0 +1,15 @@
+export const listFileType = [
+  "image/png",
+  "image/jpeg",
+  "image/jpg",
+  "application/pdf",
+  "application/vnd.ms-powerpoint",
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+  "application/vnd.rar",
+  "text/plain",
+  "application/x-7z-compressed",
+  "application/zip",
+  "audio/mpeg",
+  "video/x-ms-wmv",
+  "video/mp4",
+];
