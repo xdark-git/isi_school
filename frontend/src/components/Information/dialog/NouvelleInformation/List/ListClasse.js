e699b316f53d741c3a238edc06b24b827ac18d61
xdark-git <konteyemouhamadou@gmail.com>
2022-10-08 15:26:19 +0000

    building and updating template

--- frontend/src/components/Information/dialog/NouvelleInformation/List/ListClasse.js
+++ frontend/src/components/Information/dialog/NouvelleInformation/List/ListClasse.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 const ListClasse = (props) => {
   // console.log(props?.classe);
--- frontend/src/components/Information/dialog/NouvelleInformation/NouvelleInformation.js
+++ frontend/src/components/Information/dialog/NouvelleInformation/NouvelleInformation.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./css/style.css";
 import ListClasse from "./List/ListClasse";
+import ListFilesSelected from "./List/ListFilesSelected";
 
 const NouvelleInformation = () => {
   const [destinataires, setDestinataires] = useState("une classe");
@@ -11,9 +12,9 @@ const NouvelleInformation = () => {
     if (target.value === "") setDestinataires(null);
     else setDestinataires(target.value);
 
-    if (target.value != "une classe") setClasseName(null);
-    if (target.value != "une personne") setEmail(null);
-    if (target.value != "tout le monde") setToEveryone(false);
+    if (target.value !== "une classe") setClasseName(null);
+    if (target.value !== "une personne") setEmail(null);
+    if (target.value !== "tout le monde") setToEveryone(false);
     else setToEveryone(true);
   }, []);
 
@@ -22,6 +23,29 @@ const NouvelleInformation = () => {
   const [ClasseId, setClasseId] = useState(null);
   const [email, setEmail] = useState(null);
   const [toEveryone, setToEveryone] = useState(false);
+  const [objet, setObjet] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [files, setFiles] = useState([]);
+  useEffect(() => {
+    description != null
+      ? (document.getElementById("txt-description").textContent = description)
+      : (document.getElementById("txt-description").textContent = "");
+  }, [description]);
+
+  /*
+      we create a new input type file which is going
+      to open a window on click
+   */
+  const getFile = useCallback(() => {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.onchange = (e) => {
+      setFiles([...files, e.target.files[0]]);
+    };
+    input.click();
+
+    // eslint-disable-next-line
+  }, []);
 
   /*
     this const is going to be passed as props to ListClasse component
@@ -64,6 +88,10 @@ const NouvelleInformation = () => {
                       id="txt-destinaire-email"
                       name="email"
                       type="text"
+                      value={email !== null ? email : ""}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
                       placeholder="&Agrave;"
                     />
                   </div>
@@ -100,13 +128,33 @@ const NouvelleInformation = () => {
               <></>
             )}
             <div className="objet">
-              <input type="text" id="txt-objet" name="objet" placeholder="Objet" />
+              <input
+                type="text"
+                id="txt-objet"
+                name="objet"
+                placeholder="Objet"
+                value={objet !== null ? objet : ""}
+                onChange={(e) => setObjet(e.target.value)}
+              />
             </div>
           </div>
-          <div>
-            <div className="text"></div>
-            <div className="files"></div>
+          <div className="body-content">
+            <div
+              id="txt-description"
+              className="text"
+              contentEditable="true"
+              onInput={(e) => {
+                setDescription(e.currentTarget.textContent);
+              }}
+            ></div>
+            <div className="files">
+              <ListFilesSelected />
+            </div>
           </div>
+        </div>
+        <div className="button">
+          <i className="fa-solid fa-paperclip" onClick={getFile}></i>
+          <button className="btn ajouter">Ajouter</button>
         </div>
       </div>
     </div>
