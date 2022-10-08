3b7ac564185d65a458c13a22fb8784f9ed531dd7
xdark-git <konteyemouhamadou@gmail.com>
2022-10-08 15:25:39 +0000

    new component to list all files selected

---
+++ frontend/src/components/Information/dialog/NouvelleInformation/List/ListFilesSelected.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ListFilesSelected = () => {
+  return (
+    <>
+      <div>
+        <div>exemple.pdf</div>
+        <i className="fa-solid fa-xmark"></i>
+      </div>
+      <div>
+        <div>exemple.pdf</div>
+        <i className="fa-solid fa-xmark"></i>
+      </div>
+      <div>
+        <div>exemple.pdf</div>
+        <i className="fa-solid fa-xmark"></i>
+      </div>
+    </>
+  );
+};
+export default ListFilesSelected;
