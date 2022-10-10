04154c912dcba195ad030cc7044c1ff8a1c544ed
xdark-git <konteyemouhamadou@gmail.com>
2022-10-10 18:16:51 +0000

    navigation through children component

--- frontend/src/components/Administration/Administration.js
+++ frontend/src/components/Administration/Administration.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../navbar/Navbar";
 import "./asset/css/style.css";
+import Classes from "./children/Classes/Classes";
 import Utilisateurs from "./children/Utilisateurs/Utilisateurs";
 
 const Administration = () => {
+  const [children, setChildren] = useState({
+    Utilisateurs: true,
+    Classes: false,
+  });
   return (
     <main>
       <Navbar />
       <div className="admnistration-component">
         <div className="background-shape">
           <div className="header">
-            <div className="utilisateurs focused">Utilisateurs</div>
-            <div className="classes">Classes</div>
+            <div
+              className={children?.Utilisateurs === true ? "utilisateurs focused" : "utilisateurs"}
+              onClick={() =>
+                setChildren({
+                  Utilisateurs: true,
+                  Classes: false,
+                })
+              }
+            >
+              Utilisateurs
+            </div>
+            <div
+              className={children?.Classes === true ? "classes focused" : "classes"}
+              onClick={() =>
+                setChildren({
+                  Utilisateurs: false,
+                  Classes: true,
+                })
+              }
+            >
+              Classes
+            </div>
             <div className="line"></div>
           </div>
           <div className="body">
-            <Utilisateurs />
+            {children?.Utilisateurs === true && <Utilisateurs />}
+            {children?.Classes === true && <Classes />}
           </div>
         </div>
       </div>
