fa5b6495b1b50dbd5944bdb0049426a03b3d1860
xdark-git <konteyemouhamadou@gmail.com>
2022-10-13 17:56:16 +0000

    new action methodes

--- frontend/src/actions/users/crudUsers.js
+++ frontend/src/actions/users/crudUsers.js
@@ -5,10 +5,49 @@ export const getAllUsersAction = () => async (dispatch) => {
   try {
     const users = await api.getAllUsers();
     const data = users?.data;
-   
+
     dispatch({ type: GET_ALL_USERS, data: data });
     dispatch({ type: closeLoaderComponent });
   } catch (error) {
+    dispatch({ type: openAlertDialog, message: "Erreur survenue" });
+  }
+};
+
+export const signUpEtudiantAction = (formData) => async (dispatch) => {
+  try {
+    // eslint-disable-next-line
+    const newUser = await api.signUpEtudiant(formData);
+    dispatch({ type: openAlertDialog, message: "Utilisateur créé" });
+    dispatch({ type: closeLoaderComponent });
+  } catch (error) {
+    dispatch({ type: closeLoaderComponent });
+    // if (error?.response?.status === 409)
+    dispatch({ type: openAlertDialog, message: "Erreur survenue" });
+  }
+};
+export const signUpProfesseurAction = (formData) => async (dispatch) => {
+  try {
+    // eslint-disable-next-line
+    const newUser = await api.signUpProfesseur(formData);
+    dispatch({ type: openAlertDialog, message: "Utilisateur créé" });
+    dispatch({ type: closeLoaderComponent });
+  } catch (error) {
+    dispatch({ type: closeLoaderComponent });
+    // if (error?.response?.status === 409)
+    // console.log(error?.reponse?.data);
+    dispatch({ type: openAlertDialog, message: "Erreur survenue" });
+  }
+};
+export const signUpAdministrateurAction = (formData) => async (dispatch) => {
+  try {
+    // eslint-disable-next-line
+    const newUser = await api.signUpAdministrateur(formData);
+    dispatch({ type: openAlertDialog, message: "Utilisateur créé" });
+    dispatch({ type: closeLoaderComponent });
+  } catch (error) {
+    dispatch({ type: closeLoaderComponent });
+    // if (error?.response?.status === 409)
+    // console.log(error?.reponse?.data);
     dispatch({ type: openAlertDialog, message: "Erreur survenue" });
   }
 };
