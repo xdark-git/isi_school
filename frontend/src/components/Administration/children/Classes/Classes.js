c54137371f70daf272fd20f5957a0f205b06f1bd
xdark-git <konteyemouhamadou@gmail.com>
2022-10-25 23:02:59 +0000

    fetching and listing all classes

--- frontend/src/components/Administration/children/Classes/Classes.js
+++ frontend/src/components/Administration/children/Classes/Classes.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { openNewClassDialog } from "../../../../constantes";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAll } from "../../../../actions/classe/getClasses";
+import {
+  loaderComponentOpened,
+  openLoaderComponent,
+  openNewClassDialog,
+} from "../../../../constantes";
+import Loading from "../../../Loading/Loading";
 import "./css/style.css";
 
 const Classes = () => {
   const dispatch = useDispatch();
+
+  const classes = useSelector((state) => state?.classes?.data);
+  const isLoading = useSelector((state) => state?.isLoading?.loader);
+
+  useEffect(() => {
+    dispatch({ type: openLoaderComponent });
+    dispatch(getAll());
+
+    // eslint-disable-next-line
+  }, []);
+  const listOfClasses = useRef();
+  listOfClasses.current = classes.map((el, index) => {
+    return (
+      <tr key={index}>
+        <td>
+          <input type="checkbox" />
+          <i className="fa-solid fa-pencil"></i>
+        </td>
+        <td>{el?.nom}</td>
+        <td>{el?.profs_id.length}</td>
+        <td>{el?.etudiants_id.length}</td>
+      </tr>
+    );
+  });
   return (
     <>
       <div className="classes-btn-search">
@@ -21,36 +51,21 @@ const Classes = () => {
         </div>
       </div>
       <div className="list-classes">
-        <table>
-          <thead>
-            <tr>
-              <th className="tb-input"></th>
-              <th className="tb-nom">Nom</th>
-              <th className="tb-nbrProfesseur">Totals professeurs</th>
-              <th className="tb-nbrProfesseur">Totals étudiants</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <input type="checkbox" />
-                <i className="fa-solid fa-pencil"></i>
-              </td>
-              <td>Licence 1 Génie Logiciel</td>
-              <td>30</td>
-              <td>30</td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" />
-                <i className="fa-solid fa-pencil"></i>
-              </td>
-              <td>Licence 1 Génie Logiciel</td>
-              <td>30</td>
-              <td>30</td>
-            </tr>
-          </tbody>
-        </table>
+        {isLoading === loaderComponentOpened ? (
+          <Loading />
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th className="tb-input"></th>
+                <th className="tb-nom">Nom</th>
+                <th className="tb-nbrProfesseur">Totals professeurs</th>
+                <th className="tb-nbrProfesseur">Totals étudiants</th>
+              </tr>
+            </thead>
+            <tbody>{listOfClasses.current}</tbody>
+          </table>
+        )}
       </div>
     </>
   );
